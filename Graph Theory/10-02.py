bf7dc025997afbece11894b79cff3e5b24a58727
Seungwan Yoo <ysw99@kakao.com>
2021-09-14 01:11:38 +0900

    Update 10-02.py

--- Graph Theory/10-02.py
+++ Graph Theory/10-02.py
@@ -12,7 +12,7 @@ def find_parent(parent, x):
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
-# 두 원소가 속한 집합을 합치기는 함수
+# 두 원소가 속한 집합을 합치는 함수
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
